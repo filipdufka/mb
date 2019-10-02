d4769a213baf9bf3e797f3528dea3f849bad33a1
filipdufka-ntb <filipdufka@gmail.com>
2019-10-02 16:59:11 +0200

    Dragable Phasors

--- common/arrow.js
+++ common/arrow.js
@@ -1,13 +1,14 @@
-function drawArrow(base, vec, myColor) {
+function drawArrow(base, end, myColor) {
     push();
     stroke(myColor);
     strokeWeight(3);
     fill(myColor);
+    let vec = p5.Vector.sub(end, base);
     translate(base.x, base.y);
     line(0, 0, vec.x, vec.y);
     rotate(vec.heading());
-    let arrowSize = 7;
+    let arrowSize = 14;
     translate(vec.mag() - arrowSize, 0);
-    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+    triangle(0, arrowSize / 3, 0, -arrowSize / 3, arrowSize, 0);
     pop();
   }
--- mb_phasors/sketch.js
+++ mb_phasors/sketch.js
@@ -1,5 +1,6 @@
-let phasorA, phasorB;
-let guides;
+let phasorA, phasorB,phasorSum;
+let guides, mousePos;
+let selectedPhasor;
 
 function setup() {
 	createCanvas(900, 900);
@@ -8,12 +9,23 @@ function setup() {
 	guides.addVertical(width/2);
 
 	phasorA = new Phasor(createVector(-100,-100), 'darkblue');
+	phasorB = new Phasor(createVector(200,-100), 'red');
+	phasorSum = new Phasor(createVector(0, 0), 'green');
 }
 
 function draw() {
+	clear();
+	mousePos = createVector(mouseX, mouseY);
 	strokeWeight(1);
 	guides.show();
+
 	phasorA.show();
+	phasorB.show();
+	if(selectedPhasor != null){
+		selectedPhasor.setMouseDir();
+	}
+	phasorSum.v = p5.Vector.add(phasorA.v, phasorB.v);
+	phasorSum.show();
 }
 
 class Phasor{	
@@ -21,16 +33,39 @@ class Phasor{
 		this.v = v;
 		this.col = col;
 		this.center = createVector(guides.vs[0], guides.hs[0]);
+		this.hover = false;	
 	}
 
 	show(){
 		this.getHover();
+		
+		drawArrow(this.center, this.getEnd(), this.hover?color(125,200,255):this.col);
+	}
 
-		drawArrow(this.center, this.v), this.col;
+	getEnd(){
+		return p5.Vector.add(this.center, this.v);
+	}
+
+	setMouseDir(){
+		this.v = p5.Vector.sub(mousePos, this.center);
+		if(this.v.mag() < 30){
+			this.v.setMag(30);	
+		}
 	}
 
 	getHover(){
-		let mousePos = createVector(mouseX, mouseY);
-		//distToSegment(mousePos, )
+		let dist = distToSegment(mousePos, this.center, this.getEnd());
+
+		if(dist < 5 && this.hover == false && selectedPhasor == null){
+			this.hover = true;
+			if(mouseIsPressed){
+				  selectedPhasor = this;                      
+			}
+		}else{
+				this.hover = false;                  
+		}
+		if(mouseIsPressed == false){
+				selectedPhasor = null;
+		}
 	}
 }
