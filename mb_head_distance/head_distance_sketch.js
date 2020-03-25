022a3040db6553a0fe45393c5241d74962d6c67f
filipdufka-ntb <filipdufka@gmail.com>
2020-03-25 11:12:11 +0100

    Head Distance Labels

--- mb_head_distance/head_distance_sketch.js
+++ mb_head_distance/head_distance_sketch.js
@@ -2,6 +2,8 @@ var head_distance_sketch = function(p){
 	let soundSource, gs;
 	let headPos, headRadius, earsPos ;
 	let currentDrawColor;
+	let headDiameter = 17;
+	let magnify;
 
 	// zobrazit vzdálenosti
 	// počítat s tím, že hlava je 17cm
@@ -15,7 +17,7 @@ var head_distance_sketch = function(p){
 		earsPos = {left: new p5.Vector(headPos.x - headRadius, headPos.y), right: new p5.Vector(headPos.x + headRadius, headPos.y)};
 
 		soundSource = new DraggablePoint(new p5.Vector(headPos.x, headPos.y/2));
-
+		magnify = headDiameter / ( 2 * headRadius);
 	}
 
 	p.draw = function() {
@@ -27,6 +29,11 @@ var head_distance_sketch = function(p){
 		p.stroke(30);
 		p.fill(30) ;	
 		soundSource.show(p);
+
+		p.fill(200);
+		p.noStroke();
+		p.textAlign(p.CENTER);
+		p.text(headDiameter + " cm", headPos.x, headPos.y + 3);
 	}
 
 	p.drawLineToCenter = function(){
@@ -65,12 +72,12 @@ var head_distance_sketch = function(p){
 
 	p.drawPath = function(){	
 		p.currentDrawColor = p.color(30,120,190);
-		p.drawShortestDistance(earsPos.left);
+		p.drawShortestDistance(earsPos.left, earsPos.right);
 		p.currentDrawColor = p.color(190,30,120);
-		p.drawShortestDistance(earsPos.right);
+		p.drawShortestDistance(earsPos.right, earsPos.left);
 	}
 
-	p.drawShortestDistance = function(earPos){
+	p.drawShortestDistance = function(earPos, otherEarPos){
 		p.stroke(p.currentDrawColor);
 
 		let sidePoints = p.getSidePoints();
@@ -78,13 +85,19 @@ var head_distance_sketch = function(p){
 
 		p.strokeWeight(6);
 
+		let closestHeadPoint;
+		let distance = 0;
+
 		if(earPos.dist(soundSource.pos) <= closestSidePoint.dist(soundSource.pos)){
 			// Ear is Close
+			closestHeadPoint = earPos;
+			distance = closestHeadPoint.dist(soundSource.pos) * magnify;
 			p.line(earPos.x, earPos.y, soundSource.pos.x, soundSource.pos.y);			
 		}else{
 			// Ear is Far
+			closestHeadPoint = closestSidePoint;
 			p.line(closestSidePoint.x, closestSidePoint.y, soundSource.pos.x, soundSource.pos.y);
-			let zeroVec = new p5.Vector(-1,0);
+
 			let closestSideDir = p5.Vector.sub(closestSidePoint, headPos);
 			let angleSide = closestSideDir.heading();
 			
@@ -92,20 +105,33 @@ var head_distance_sketch = function(p){
 			angleEar = earDir.heading();
 
 			if((angleEar - angleSide) > Math.PI){ angleSide = p.cleanAngle(angleSide);	}
+
+			//compute distance
+			shadowAngle = Math.max(angleSide, angleEar) - Math.min(angleEar, angleSide);
+			shadowDistance = shadowAngle * headDiameter / 2;
+			distance = closestSidePoint.dist(soundSource.pos) * magnify + shadowDistance;
+
 			p.noFill();
 			p.arc(headPos.x, headPos.y, headRadius * 2, headRadius * 2, Math.min(angleEar, angleSide), Math.max(angleSide, angleEar));			
 		}
 		// Draw Normal
-		let midPoint = p5.Vector.lerp(closestSidePoint, soundSource.pos, 0.5);
-		let normal = getNormal(closestSidePoint, soundSource.pos).mult(10);
-		p.line(midPoint.x, midPoint.y, midPoint.x + normal.x, midPoint.y + normal.y);
+		let midPoint = p5.Vector.lerp(closestHeadPoint, soundSource.pos, 0.5);
+		let normal = getNormal(closestSidePoint, soundSource.pos).mult(20);
+		let side = Math.sign((soundSource.pos.x - closestHeadPoint.x) * (otherEarPos.y - closestHeadPoint.y) - (soundSource.pos.y - closestHeadPoint.y) * (otherEarPos.x - closestHeadPoint.x));
+		normal.mult(side);
+		//p.line(midPoint.x, midPoint.y, midPoint.x + normal.x, midPoint.y + normal.y);
 		
 
 		// Text draw
 		p.noStroke();		
 		p.fill(p.currentDrawColor);
-		
-		p.text(earPos.x, midPoint.x, midPoint.y );
+		p.textAlign(p.RIGHT);
+		if(normal.x > 0){
+			p.textAlign(p.LEFT);
+		}
+
+		distance = Math.round(distance * 10) / 10;
+		p.text(distance + " cm", midPoint.x + normal.x, midPoint.y + normal.y);
 
 		
 
@@ -114,6 +140,7 @@ var head_distance_sketch = function(p){
 	p.drawHead = function(){
 		//head
 		p.noStroke();
+		p.fill(30);
 		p.circle(headPos.x, headPos.y, headRadius * 2);
 		p.circle(earsPos.left.x, earsPos.left.y, headRadius / 2);
 		p.circle(earsPos.right.x, earsPos.right.y, headRadius / 2);
