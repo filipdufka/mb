f0853fcfe202c0c902fe34a353672c3d66d3489c
sebous <sebous@gmail.com>
2020-05-26 10:29:33 +0200

    fixed P5Wrapper mount/unmount lifecycle method

--- src/utils/react-p5-wrapper.tsx
+++ src/utils/react-p5-wrapper.tsx
@@ -1,23 +1,33 @@
 import p5 from "p5";
-import React, { useState, useRef, Component } from "react";
-
+import React, { useState, useRef } from "react";
 
 export interface P5WrapperProps {
   sketch: (p: p5) => void;
   sketchProps?: object;
 }
 
-export const P5Wrapper: React.FC<P5WrapperProps> = (props: P5WrapperProps) => {
+export const P5Wrapper: React.FC<P5WrapperProps> = ({ sketch, sketchProps }) => {
   const wrapper = useRef(null);
-  const [canvas, setCanvas] = useState(new p5(props.sketch, wrapper.current));
+  const [canvas, setCanvas] = useState<p5 | null>(null);
 
-  React.useEffect(() => { return () => { canvas.remove(); } }, []);
-  //React.useEffect(() => { console.log("Props changed."); canvas.remove(); }, [props.sketchProps]);
+  const createCanvas = () => {
+    console.log("creating canvas...");
+    canvas?.remove();
+    setCanvas(new p5(sketch, wrapper.current));
+  };
 
-  return <div ref={wrapper} style={{ position: 'fixed' }}></div>;
+  React.useEffect(() => {
+    if (!canvas) {
+      createCanvas();
+    }
+    return () => {
+      console.log("removing");
+      canvas?.remove();
+    };
+  }, [wrapper]);
+
+  return <div ref={wrapper} style={{ position: "fixed" }}></div>;
 };
-
-
 
 // export interface P5WrapperState {
 //   sketch: (p: p5) => void;
@@ -43,16 +53,14 @@ export const P5Wrapper: React.FC<P5WrapperProps> = (props: P5WrapperProps) => {
 //     this.setState({
 //       canvas: canvas,
 //       wrapper: this.wrapper
-//     })    
+//     })
 //   }
-
-
 
 //   componentWillUnmount() {
 // 		this.state.canvas.remove();
 // 	}
 
-//   render() {    
+//   render() {
 //     return <div ref={wrapper => this.wrapper = wrapper} style={{position: 'fixed'}}></div>;
 //   }
 // }
