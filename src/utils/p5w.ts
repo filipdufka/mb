97a62de1ae0c24c9cf92aa4f2f7c67fc04a1d443
filipdufka-surka-pc <dufka@ssudbrno.cz>
2020-06-03 13:38:06 +0200

    Remove Garbage

--- src/utils/p5w.ts
+++ src/utils/p5w.ts
@@ -1,27 +1,5 @@
-import p5, { p5InstanceExtensions, AXES } from "p5";
+import p5 from 'p5';
 
-export class P5w {
-  private p5: p5;
-
-  constructor(whatevs: any) {
-    // construct p5 instance here
-    // this.p5 = new p5(....)
-  }
-
-  // this will return actual p5 object
-  public unwrap() {
-    return this.p5;
-  }
-
-  public updateProps(props: Partial<p5InstanceExtensions>) {
-    Object.entries(props).forEach(([key, value]) => (this.p5[key] = value));
-    // maybe do something here after update?
-  }
+export class P5w<T> extends p5 {
+  updateProps = (props : T) => {}
 }
-
-const controlledP5 = new P5w("dontcare");
-
-// type checking works
-controlledP5.updateProps({ AXES: "axes" });
-
-const actualP5instance = controlledP5.unwrap();
